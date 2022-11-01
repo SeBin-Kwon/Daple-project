5cd35a939adbeca5c2220acbe4f2b66dc2b519a0
yodayeong <duekdud01@naver.com>
2022-11-01 17:41:58 +0900

    register/login/mypage complete

--- accounts/urls.py
+++ accounts/urls.py
@@ -22,4 +22,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('mypage/<int:pk>/', views.mypage, name='mypage'),
 ]
