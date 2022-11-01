daf6d97fcac36998a277dbee2019f72f33e06026
yodayeong <duekdud01@naver.com>
2022-11-01 10:21:57 +0900

    accounts develop

--- daple/settings.py
+++ daple/settings.py
@@ -78,11 +78,11 @@ DATABASES = {
         'ENFORCE_SCHEMA': False,
         "NAME": "test",  # name of your DB which you want to access
         "CLIENT": {
-            'host': 'mongodb+srv://<username>:<userpassword>4@model.qbltslw.mongodb.net/?retryWrites=true&w=majority',
+            'host': 'mongodb+srv://dayeong:hihello01!4@model.qbltslw.mongodb.net/?retryWrites=true&w=majority',
             # your db_url if not hosted then localhost
             'port': 27017,  # port e.g. 27017
-            'username': '<username>',
-            'password': '<userpassword>',
+            'username': 'dayeong',
+            'password': 'hihello01!',
             'authSource': 'admin',  # set your db auth_source if you know
             'authMechanism': 'SCRAM-SHA-1'  # set your auth_mechanism if you know
             # add other settings as per your requirements
