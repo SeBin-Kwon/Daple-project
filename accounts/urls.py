d79f29d0b37cc7c7fec62196fd0d78eded65aaf4
yodayeong <duekdud01@naver.com>
2022-11-01 16:27:10 +0900

    revising login

--- accounts/urls.py
+++ accounts/urls.py
@@ -19,5 +19,7 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('signup/', views.signup, name='signup')
+    path('', views.index, name='index'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 
 # Create your views here.
+def index(request):
+    return render(request, 'accounts/index.html')
+
 def signup(request):
-    return render(request, 'accounts/signup.html')
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('accounts:index')
+    else:
+        form = CustomUserCreationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/signup.html', context)
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('accounts:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/login.html', context)
