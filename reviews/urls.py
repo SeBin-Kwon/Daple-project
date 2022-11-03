d98d1606964e20c5fc7db8704c84edec6dc27a48
wnsn8546 <wnsn8546@gmail.com>
2022-11-03 10:13:49 +0900

    comments complete

--- reviews/urls.py
+++ reviews/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path('create/', views.create, name='create'),
     path('<int:pk>/delete/', views.delete, name='delete'),
     path('<int:pk>/update/', views.update, name='update'),
+    path('<int:pk>/comments/', views.comment_create, name='comment_create'),
+    path('<int:pk>/<int:comment_pk>/update/', views.comment_update, name='comment_update'),
+    path('<int:pk>/<int:comment_pk>/delete/', views.comment_delete, name='comment_delete'),
+    path('<int:pk>/like/', views.like, name='like'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,25 +1,51 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
 from .models import Review, Comment
 from .forms import ReviewForm, CommentForm
+from django.http import JsonResponse
+import json
+import datetime
 # Create your views here.
 
 def index(request):
     reviews = Review.objects.order_by('-pk')
+    comments = Comment.objects.all().order_by('-pk')
+    comment_form = CommentForm()
     context = {
-        'reviews' : reviews
+        'reviews' : reviews,
+        'comment_form' : comment_form,
+        'comments':comments,
     }
     return render(request, 'reviews/index.html', context)
 
 def create(request):
     if request.method == 'POST':
-        form = ReviewForm(request.POST,request.FILES)
-        if form.is_valid():
-            form.save()
+        review_form = ReviewForm(request.POST,request.FILES)
+        if review_form.is_valid():
+            review = review_form.save(commit=False)
+            review.user = request.user
+            review_form.save()
             return redirect('reviews:index')
     else:
-        form = ReviewForm()
+        review_form = ReviewForm()
     context = {
-        'form' : form
+        'review_form' : review_form,
+    }
+    return render(request, 'reviews/form.html', context)
+
+def update(request, pk):
+    review = Review.objects.get(pk=pk)
+    if request.method == 'POST':
+        review_form = ReviewForm(request.POST, request.FILES, instance=review)
+        if review_form.is_valid():
+            reviews = review_form.save(commit=False)
+            reviews.user = request.user
+            reviews.save()
+            return redirect('reviews:index')
+    else:
+        review_form = ReviewForm(instance=review)
+    context = {
+        'review_form' : review_form
     }
     return render(request, 'reviews/form.html', context)
 
@@ -27,52 +53,97 @@ def delete(request, pk):
     Review.objects.get(pk=pk).delete()
     return redirect('reviews:index')
 
-def update(request, pk):
-    review = Review.objects.get(pk=pk)
-    if request.method == 'POST':
-        form = ReviewForm(request.POST, request.FILES, instance=review)
-        if form.is_valid():
-            reviews = form.save(commit=False)
-            reviews.user = request.user
-            reviews.save()
-            return redirect('reviews:index')
-    else:
-        form = ReviewForm(instance=review)
-    context = {
-        'form' : form
-    }
-    return render(request, 'reviews/form.html', context)
 
 def comment_create(request, pk):
-    if request.user.is_authenticated:
-        reviews = get_object_or_404(Review, pk=pk)
-        comment_form = CommentForm(request.POST)
-        if comment_form.is_valid():
-            comment = comment_form.save(commit=False)
-            comment.review = reviews
-            comment.user = request.user
-            comment.save()
-        return redirect('reviews:index')
-    return redirect('accounts:login')
+    review = get_object_or_404(Review, pk=pk)
+    comment_form = CommentForm(request.POST)
+    if comment_form.is_valid():
+        comment = comment_form.save(commit=False)
+        comment.review = review
+        comment.user = request.user
+        comment.save()
+        
+        comments = Comment.objects.filter(review_id=pk).order_by('-pk')
+        comments_data = []
+        for co in comments:
+            co.created_at = co.created_at.strftime('%Y-%m-%d %H:%M')
+            comments_data.append(
+                {
+                    'request_user_pk': request.user.pk,
+                    'comment_pk': co.pk,
+                    'user_pk': co.user.pk,
+                    'username': co.user.username,
+                    'content': co.comment_content,
+                    'created_at': co.created_at,
+                    'updated_at':co.updated_at,
+                    'review_pk':co.review_id,
+                })
+        context = {
+            'comments_data':comments_data
+        }
+        return JsonResponse(context)
 
-def comment_update(request, comment_pk):
+def comment_update(request, pk, comment_pk):
     if request.user.is_authenticated:
+        jsonObject = json.loads(request.body)
+
         comment = Comment.objects.get(pk=comment_pk)
-        comment_form = CommentForm(request.POST, instance=comment)
+        comment.comment_content = jsonObject.get('content')
+        comment.save()
 
-        if request.method == "POST":
-            if request.user == comment.user:
-                if comment_form.is_valid():
-                    comment_form.save()
+        comments = Comment.objects.filter(review_id=pk).order_by('-pk')
+        comments_data = []
+        for co in comments:
+            co.created_at = co.created_at.strftime('%Y-%m-%d %H:%M')
+            comments_data.append(
+                {
+                    'request_user_pk': request.user.pk,
+                    'comment_pk': co.pk,
+                    'user_pk': co.user.pk,
+                    'username': co.user.username,
+                    'content': co.comment_content,
+                    'created_at': co.created_at,
+                    'updated_at':co.updated_at,
+                    'review_pk':co.review_id,
+                })
+        context = {
+            'comments_data':comments_data
+        }
+        return JsonResponse(context)
 
-
-        return redirect('reviews:index')
-    return redirect('accounts:login')
-
-def comment_delete(request, comment_pk):
+def comment_delete(request, pk, comment_pk):
     if request.user.is_authenticated:
         comment = Comment.objects.get(pk=comment_pk)
         comment.delete()
-        return redirect('reviews:index')
-    return redirect('accounts:login')
+        
+        comments = Comment.objects.filter(review_id=pk).order_by('-pk')
+        comments_data = []
+        for co in comments:
+            co.created_at = co.created_at.strftime('%Y-%m-%d %H:%M')
+            comments_data.append(
+                {   'request_user_pk': request.user.pk,
+                    'comment_pk': co.pk,
+                    'user_pk': co.user.pk,
+                    'username': co.user.username,
+                    'content': co.comment_content,
+                    'created_at': co.created_at,
+                    'updated_at':co.updated_at,
+                    'review_pk':co.review_id,
+                })
+        context = {
+            'comments_data':comments_data
+        }
+        return JsonResponse(context)
 
+def like(request, pk):
+    review = get_object_or_404(Review, pk=pk)
+    
+    if review.review_liked.filter(id=request.user.id).exists():
+    # if request.user in review.review_liked.all(): 
+        review.review_liked.remove(request.user)
+        is_liked = False
+    else:
+        review.review_liked.add(request.user)
+        is_liked = True
+    context = {'isLiked': is_liked, 'likeCount': review.review_liked.count()}
+    return JsonResponse(context)
