0c67d71b3fa5b5fceb2d571aaf00f53873c8ee05
yodayeong <duekdud01@naver.com>
2022-11-03 17:15:58 +0900

    mypage_update revise

--- accounts/urls.py
+++ accounts/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('callback/', views.callback, name='callback'),
     path('logout/', views.logout, name='logout'),
     path('mypage/<int:pk>/', views.mypage, name='mypage'),
+    path('password/', views.change_password, name='change_password'),
     path('mypage-update/<int:pk>/', views.mypage_update, name='mypage-update'),
     path('mypage-delete/<int:pk>/', views.mypage_delete, name='mypage-delete'),
     path('temp/<int:pk>/', views.temp, name='temp'),
