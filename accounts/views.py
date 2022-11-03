d4229c4d054181a7469bf56a796949fef56ea498
runedemonic <runedemonic@naver.com>
2022-11-03 22:01:19 +0900

    kakao map API complete

--- accounts/views.py
+++ accounts/views.py
@@ -6,9 +6,30 @@ from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import update_session_auth_hash
-
+from stores.models import Foodtag, Thematag
 # Create your views here.
 def index(request):
+    Thematag.objects.create(thematag_name='가성비좋은')
+    Thematag.objects.create(thematag_name='분위기좋은')
+    Thematag.objects.create(thematag_name='푸짐한')
+    Thematag.objects.create(thematag_name='격식있는')
+    Thematag.objects.create(thematag_name='고급스러운')
+    Thematag.objects.create(thematag_name='서민적인')
+    Thematag.objects.create(thematag_name='시끌벅적한')
+    Thematag.objects.create(thematag_name='조용한')
+    Thematag.objects.create(thematag_name='깔끔한')
+    Thematag.objects.create(thematag_name='이색적인')
+    Thematag.objects.create(thematag_name='뷰가좋은')
+    Thematag.objects.create(thematag_name='예쁜')
+    Thematag.objects.create(thematag_name='지역주민이찾는')
+
+    Foodtag.objects.create(foodtag_name='한식')
+    Foodtag.objects.create(foodtag_name='중식')
+    Foodtag.objects.create(foodtag_name='양식')
+    Foodtag.objects.create(foodtag_name='일식')
+    Foodtag.objects.create(foodtag_name='아시안')
+    Foodtag.objects.create(foodtag_name='패스트푸드')
+
     return render(request, 'accounts/index.html')
 
 def signup(request):
