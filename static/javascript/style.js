a0fd228dbb13f330b87c316d11328803106acd41
sebin kwon <sbkwon98@gmail.com>
2022-11-07 15:51:44 +0900

    hotfix map

--- static/javascript/style.js
+++ static/javascript/style.js
@@ -50,33 +50,33 @@
 //         }
 //     })
 
-var swiper = new Swiper('.swiper-container', {
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  slidesPerView: 1,  //초기값 설정 모바일값이 먼저!!
-  spaceBetween: 10,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  breakpoints: {
+// var swiper = new Swiper('.swiper-container', {
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+//   slidesPerView: 1,  //초기값 설정 모바일값이 먼저!!
+//   spaceBetween: 10,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   breakpoints: {
   
-    768: {
-      slidesPerView: 2,  //브라우저가 768보다 클 때
-      spaceBetween: 20,
-    },
-    1024: {
-      slidesPerView: 3,  //브라우저가 1024보다 클 때
-      spaceBetween: 30,
-    },
-    3000: {
-      slidesPerView: 4,  
-      spaceBetween: 0,
-    }
-  },
-  autoplay:{
-      delay: 10000,
-  }
-});
+//     768: {
+//       slidesPerView: 2,  //브라우저가 768보다 클 때
+//       spaceBetween: 20,
+//     },
+//     1024: {
+//       slidesPerView: 3,  //브라우저가 1024보다 클 때
+//       spaceBetween: 30,
+//     },
+//     3000: {
+//       slidesPerView: 4,  
+//       spaceBetween: 0,
+//     }
+//   },
+//   autoplay:{
+//       delay: 10000,
+//   }
+// });
